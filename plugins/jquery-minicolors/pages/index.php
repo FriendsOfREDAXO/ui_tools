0ee7b83a9c9055001310a8519d15388ef1a3801d
Tim Filler <t.filler@drei-elemente.com>
2017-08-22 23:16:11 +0200

    changes to the plugin pages

--- plugins/jquery-minicolors/pages/index.php
+++ plugins/jquery-minicolors/pages/index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo rex_view::title(rex_i18n::msg('jquery-minicolors_menu_entry'));
+//echo rex_view::title(rex_i18n::msg('jquery-minicolors_menu_entry'));
+
+$file = rex_file::get(rex_path::addon('be_utilities/plugins/jquery-minicolors/assets/jquery-minicolors','README.md'));
+$Parsedown = new Parsedown();
+$content =  '<div id="be_utilities">'.$Parsedown->text($file);
+
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', rex_i18n::msg('jquery-minicolors_info'));
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
+
+$file = rex_file::get(rex_path::addon('be_utilities/plugins/jquery-minicolors/assets/jquery-minicolors','LICENSE.md'));
+$Parsedown = new Parsedown();
+
+$content =  '<div id="be_utilities">'.$Parsedown->text($file).'</div>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', rex_i18n::msg('jquery-minicolors_license'));
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
--- plugins/selectize/pages/index.php
+++ plugins/selectize/pages/index.php
@@ -1,3 +1,34 @@
 <?php
 
-echo rex_view::title(rex_i18n::msg('selectize_menu_entry'));
+//echo rex_view::title(rex_i18n::msg('selectize_menu_entry'));
+
+$file = rex_file::get(rex_path::addon('be_utilities/plugins/selectize/assets/selectize','README.md'));
+$Parsedown = new Parsedown();
+
+$content =  '<div id="be_utilities">'.$Parsedown->text($file);
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', rex_i18n::msg('selectize_info'));
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
+
+$file = rex_file::get(rex_path::addon('be_utilities/plugins/selectize/assets/selectize','LICENSE'));
+$Parsedown = new Parsedown();
+
+$content =  '<div id="be_utilities">'.$Parsedown->text($file).'</div>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', rex_i18n::msg('selectize_license'));
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
+
+
+$file = rex_file::get(rex_path::addon('be_utilities/plugins/selectize/assets/selectize','CHANGELOG.md'));
+$Parsedown = new Parsedown();
+
+$content =  '<div id="be_utilities">'.$Parsedown->text($file).'</div>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', rex_i18n::msg('selectize_changelog'));
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
