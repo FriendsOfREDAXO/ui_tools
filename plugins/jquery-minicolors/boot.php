de4aae5674bdda8ce209eff1b12f9dbf34fdc2b0
Tim Filler <t.filler@drei-elemente.com>
2017-08-22 22:16:15 +0200

    corrections for installing the plugins

--- plugins/jquery-minicolors/boot.php
+++ plugins/jquery-minicolors/boot.php
@@ -1,6 +1,6 @@
 <?php
 
 if(rex::isBackend()) {
-    rex_view::addCssFile($this->getAssetsUrl('jquery-minicolor/jquery.minicolors.css'));
-    rex_view::addJsFile($this->getAssetsUrl('jquery-minicolor/jquery.minicolors.min.js'));
+    rex_view::addCssFile($this->getAssetsUrl('jquery-minicolors/jquery.minicolors.css'));
+    rex_view::addJsFile($this->getAssetsUrl('jquery-minicolors/jquery.minicolors.min.js'));
 }
