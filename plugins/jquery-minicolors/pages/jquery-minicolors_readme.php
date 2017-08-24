89b3b69685109d55df487493bf4fe2e9d93b367b
Tim Filler <elricco@web.de>
2017-08-24 19:19:07 +0200

    setup subpages for plugin jquery-minicolors

---
+++ plugins/jquery-minicolors/pages/jquery-minicolors_readme.php
@@ -0,0 +1,13 @@
+<?php
+
+//echo rex_view::title(rex_i18n::msg('jquery-minicolors_menu_entry'));
+
+$file = rex_file::get(rex_path::addon('be_utilities/plugins/jquery-minicolors/assets/jquery-minicolors','README.md'));
+$Parsedown = new Parsedown();
+$content =  '<div id="be_utilities">'.$Parsedown->text($file);
+
+
+$fragment = new rex_fragment();
+$fragment->setVar('title', rex_i18n::msg('jquery-minicolors_readme'));
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
