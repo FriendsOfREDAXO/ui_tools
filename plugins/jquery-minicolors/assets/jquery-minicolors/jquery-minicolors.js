d8284e6aeba75d25b9ebd0f92faa0a4829c2cc02
Tim Filler <elricco@web.de>
2017-08-27 19:41:52 +0200

    make jQuery mini colors functional again

---
+++ plugins/jquery-minicolors/assets/jquery-minicolors/jquery-minicolors.js
@@ -0,0 +1,25 @@
+$(document).on('ready pjax:success',function() {
+    settings = {
+        animationSpeed: 50,
+        animationEasing: 'swing',
+        change: null,
+        changeDelay: 0,
+        control: 'hue',
+        defaultValue: '',
+        format: 'rgb',
+        hide: null,
+        hideSpeed: 100,
+        inline: false,
+        keywords: '',
+        letterCase: 'lowercase',
+        opacity: true,
+        position: 'bottom left',
+        show: null,
+        showSpeed: 100,
+        theme: 'bootstrap',
+        swatches: []
+    };
+
+    $('input.textColor').minicolors(settings);
+    $('input.bgColor').minicolors(settings);
+});
--- plugins/jquery-minicolors/boot.php
+++ plugins/jquery-minicolors/boot.php
@@ -3,4 +3,5 @@
 if(rex::isBackend()) {
     rex_view::addCssFile($this->getAssetsUrl('jquery-minicolors/jquery.minicolors.css'));
     rex_view::addJsFile($this->getAssetsUrl('jquery-minicolors/jquery.minicolors.min.js'));
+    rex_view::addJsFile($this->getAssetsUrl('jquery-minicolors/jquery-minicolors.js'));
 }
