f6042f40a139d9102a0f541f77705a0dda6836e5
Tim Filler <elricco@web.de>
2017-09-28 00:06:59 +0200

    add colorpicker to system customizer input

---
+++ plugins/jquery-minicolors/assets/jquery-minicolors/jQuery-minicolors-for-customizer.js
@@ -0,0 +1,24 @@
+$(document).on('ready pjax:success',function() {
+    customizer_settings = {
+        animationSpeed: 50,
+        animationEasing: 'swing',
+        change: null,
+        changeDelay: 0,
+        control: 'hue',
+        defaultValue: '',
+        format: 'hex',
+        hide: null,
+        hideSpeed: 100,
+        inline: false,
+        keywords: '',
+        letterCase: 'lowercase',
+        opacity: false,
+        position: 'bottom left',
+        show: null,
+        showSpeed: 100,
+        theme: 'bootstrap',
+        swatches: []
+        };
+
+    $('input#customizer-labelcolor').minicolors(customizer_settings);
+});
--- plugins/jquery-minicolors/boot.php
+++ plugins/jquery-minicolors/boot.php
@@ -4,4 +4,5 @@ if(rex::isBackend()) {
     rex_view::addCssFile($this->getAssetsUrl('jquery-minicolors/jquery.minicolors.css'));
     rex_view::addJsFile($this->getAssetsUrl('jquery-minicolors/jquery.minicolors.min.js'));
     rex_view::addJsFile($this->getAssetsUrl('jquery-minicolors/jquery-minicolors.js'));
+    rex_view::addJsFile($this->getAssetsUrl('jquery-minicolors/jquery-minicolors-for-customizer.js'));
 }
