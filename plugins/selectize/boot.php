2658bac439a8d28e8015061266a7f95c3653e4d1
Thomas Skerbis <skerbis@klxm.de>
2022-01-24 23:27:38 +0100

    assets repair

--- plugins/selectize/boot.php
+++ plugins/selectize/boot.php
@@ -4,5 +4,5 @@ if(rex::isBackend()) {
     rex_view::addCssFile($this->getAssetsUrl('selectize/dist/css/selectize.css'));
     rex_view::addCssFile($this->getAssetsUrl('selectize/dist/css/selectize.bootstrap3.css'));
     rex_view::addJsFile($this->getAssetsUrl('selectize/dist/js/standalone/selectize.min.js'));
-    rex_view::addJsFile($this->getAssetsUrl('selectize/selectize.js'));
+    rex_view::addJsFile($this->getAssetsUrl('rex_selectize.js'));
 }
