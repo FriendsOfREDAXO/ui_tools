3f83984121d8698c629370a5fc87e3d462431746
Thomas Skerbis <thomas.skerbis@mac.com>
2022-08-16 12:21:20 +0200

    Update bootstrap-datetimepicker_less.php

--- plugins/bootstrap-datetimepicker/pages/bootstrap-datetimepicker_less.php
+++ plugins/bootstrap-datetimepicker/pages/bootstrap-datetimepicker_less.php
@@ -3,7 +3,7 @@
 //echo rex_view::title(rex_i18n::msg('bootstrap-datetimepicker_menu_entry'));
 
 
-$file = rex_file::get(rex_path::addon('ui_tools/plugins/bootstrap-datetimepicker/assets/bootstrap-datetimepicker','LESS.md'));
+$file = rex_file::get(rex_path::addon('ui_tools/plugins/bootstrap-datetimepicker','LESS.md'));
 $Parsedown = new Parsedown();
 
 $content =  '<div id="ui_tools">'.$Parsedown->text($file).'</div>';
